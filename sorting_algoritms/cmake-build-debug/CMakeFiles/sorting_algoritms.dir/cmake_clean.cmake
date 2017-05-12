9bc292e27750f1a6a80ec20aebba10495bf470bf
pdata <pdata87@o2.pl>
2017-05-12 11:54:12 +0200

    Initial commit - working sorting functions, return execution time, no OOP

---
+++ sorting_algoritms/cmake-build-debug/CMakeFiles/sorting_algoritms.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sorting_algoritms.dir/main.cpp.o"
+  "sorting_algoritms.pdb"
+  "sorting_algoritms"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sorting_algoritms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
