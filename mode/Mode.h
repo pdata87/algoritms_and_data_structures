9f57deb8a605e17efcc739a5a673edd2a0178bcd
pdata <pdata87@o2.pl>
2017-05-12 15:30:15 +0200

    Moving find_modes function to class Mode

--- mode/Mode.h
+++ mode/Mode.h
@@ -11,8 +11,12 @@ using namespace std;
 class Mode {
 
         int counter = 0;
+        vector<int> list;
         vector<int> mode_values;
     public:
+        Mode(vector<int> &list){
+            this->list = list;
+        }
         void setCounter(int value){
             this->counter = value;
         }
@@ -29,6 +33,44 @@ class Mode {
         }
         void resetModeValues(){
             this->mode_values.erase(this->mode_values.begin(),this->mode_values.end());
+        }
+        void find_modes(){
+
+            int actual_mode_counter =0;
+            int actual_mode_value;
+
+            // itercja do konca vectora
+            for(int i=0;i<this->list.size();i++){
+                int temporary_counter =0; // this counter holds information about number of repetitions of a list[i] value
+                for(int k = i;k<this->list.size();k++){
+
+                    if(list[i] == list[k]){
+                        temporary_counter++; // found repetition, increase counter
+                    }
+
+
+                }
+                if(temporary_counter>actual_mode_counter) // check if temporary counter is grater than stored counter;
+                {
+                    actual_mode_counter = temporary_counter;
+                    actual_mode_value = this->list[i];
+
+                    setCounter(temporary_counter);
+                    resetModeValues();
+                    addModeValue(list[i]);
+                    //cout << "Found new most repetitive value " << list[i] << " exist in list " << temporary_counter <<" times" <<endl;
+                }
+                if(temporary_counter==actual_mode_counter && list[i] != actual_mode_value) // found new
+                {
+                    actual_mode_counter = temporary_counter;
+                    addModeValue(this->list[i]);
+                    //cout << "Found new value "<< list[i]  << "which repreats "  << temporary_counter << " times in list "  <<endl;
+                }
+                //cout << list[i] << " found in list : " << temporary_counter <<" times " << endl;
+            }
+
+
+
         }
 
 
--- mode/main.cpp
+++ mode/main.cpp
@@ -6,18 +6,24 @@ using namespace std;
 void fill_with_random_value(vector < int > &vector_to_fill, int no_of_elements);
 void display_list_values(vector<int> &vector_to_display);
 
-Mode find_modes(vector<int> &list);
+
 
 int main() {
     int no_of_elements = 200;
-    //vector <int> list(no_of_elements) ;
-    //fill_with_random_value(list,no_of_elements);
-    vector <int> list = {1,2,3,1,1,1,1,1,1,1,1};
 
+    //random vector implementation
+    vector <int> list(no_of_elements) ;
+    fill_with_random_value(list,no_of_elements);
+
+    //static vector implementation
+    //vector <int> list = {1,2,3,4,5,6,7,1};
     display_list_values(list);
 
-    Mode modes=    find_modes(list);
-    modes.printModeInfo();
+    Mode modes (list);
+        modes.find_modes();
+        modes.printModeInfo();
+
+
     return 0;
 }
 void fill_with_random_value(vector < int > &vector_to_fill, int no_of_elements){
@@ -35,42 +41,5 @@ void display_list_values(vector<int> &vector_to_display){
     }
 
 }
-Mode find_modes(vector<int> &list){
 
-    int actual_mode_counter =0;
-    int actual_mode_value;
-    Mode modeValues;
-    // itercja do konca vectora
-    for(int i=0;i<list.size();i++){
-        int temporary_counter =0; // this counter holds information about number of repetitions of a list[i] value
-        for(int k = i;k<list.size();k++){
-
-            if(list[i] == list[k]){
-                temporary_counter++; // found repetition, increase counter
-            }
-
-
-        }
-        if(temporary_counter>actual_mode_counter) // check if temporary counter is grater than stored counter;
-        {
-            actual_mode_counter = temporary_counter;
-            actual_mode_value = list[i];
-
-            modeValues.setCounter(temporary_counter);
-            modeValues.resetModeValues();
-            modeValues.addModeValue(list[i]);
-            //cout << "Found new most repetitive value " << list[i] << " exist in list " << temporary_counter <<" times" <<endl;
-        }
-        if(temporary_counter==actual_mode_counter && list[i] != actual_mode_value) // found new
-        {
-            actual_mode_counter = temporary_counter;
-            modeValues.addModeValue(list[i]);
-            //cout << "Found new value "<< list[i]  << "which repreats "  << temporary_counter << " times in list "  <<endl;
-        }
-        //cout << list[i] << " found in list : " << temporary_counter <<" times " << endl;
-    }
-
-    return modeValues;
-
-}
 
