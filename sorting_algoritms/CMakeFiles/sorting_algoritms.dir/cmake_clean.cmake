59c0ded3faebb6ce1d496db8e774f27cee8f6bb5
pdata <pdata87@o2.pl>
2017-06-05 11:14:19 +0200

    removed some cmake files

--- sorting_algoritms/CMakeFiles/sorting_algoritms.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sorting_algoritms.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- sorting_algoritms/CMakeFiles/sorting_algoritms.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for sorting_algoritms.
-# This may be replaced when dependencies are built.
